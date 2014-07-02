6653844b078bf89a51fd4a15611578f9e6e80e54
Luigi Ruocco <luigi.ruocco@entaksi.eu>
2014-07-02 14:22:12 +0200

    Test di firma e verifica dei una FatturaPA

--- src/test/java/xades4j/verification/VerifierFatturaPATest.java
+++ src/test/java/xades4j/verification/VerifierFatturaPATest.java
@@ -3,15 +3,27 @@ package xades4j.verification;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import xades4j.algorithms.EnvelopedSignatureTransform;
+import xades4j.production.*;
+import xades4j.properties.DataObjectDesc;
+import xades4j.providers.BasicSignatureOptionsProvider;
 import xades4j.providers.CertificateValidationProvider;
+import xades4j.providers.KeyingDataProvider;
+import xades4j.providers.impl.KeyStoreKeyingDataProvider;
+import xades4j.providers.impl.PKCS11KeyStoreKeyingDataProvider;
 import xades4j.providers.impl.PKIXCertificateValidationProvider;
+import xades4j.utils.XadesProfileResolutionException;
 
 import java.io.FileInputStream;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.cert.CertStore;
 import java.security.cert.CollectionCertStoreParameters;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by luigi on 26/06/14.
@@ -48,6 +60,107 @@ public class VerifierFatturaPATest extends VerifierTestBase
         Element signatureNode = getSigElement(getDocument("fatturapa/IT01234567890_X1111.xml"));
         XAdESVerificationResult res = verificationProfile.newVerifier().verify(signatureNode, null);
         Assert.assertEquals(XAdESForm.BES, res.getSignatureForm());
+    }
+
+    @Test
+    public void testSignAndVerifyBES() throws Exception
+    {
+        System.out.println("signFatturaPA");
+
+        XadesSigner signer = getSigner();
+
+        Document docSource = getDocument("fatturapa/IT01234567890_11111.xml");
+
+        DataObjectDesc dataObjRef = new DataObjectReference("")
+                .withTransform(new EnvelopedSignatureTransform());
+        //.withTransform(XPath2FilterTransform.XPath2Filter.subtract("/descendant::ds:Signature"));
+        signer.sign(new SignedDataObjects(dataObjRef), docSource.getDocumentElement());
+
+        outputDocument(docSource, "IT01234567890_11111(signed).xml");
+
+
+        System.out.println("verifyFatturaPA");
+
+        Element signatureNode = getSigElement(getDocument("out/IT01234567890_11111(signed).xml"));
+        XAdESVerificationResult res = verificationProfile.newVerifier().verify(signatureNode, null);
+
+        System.out.println(res.getSignatureForm());
+        System.out.println(res.getSignatureAlgorithmUri());
+        System.out.println(res.getSignedDataObjects().size());
+        System.out.println(res.getQualifyingProperties().all().size());
+
+        Assert.assertEquals(XAdESForm.BES, res.getSignatureForm());
+    }
+
+    private XadesSigner getSigner()
+    {
+        KeyStoreKeyingDataProvider.SigningCertSelector scs = new KeyStoreKeyingDataProvider.SigningCertSelector()
+        {
+            @Override
+            public X509Certificate selectCertificate(List<X509Certificate> x509Certificates)
+            {
+                return x509Certificates.get(1);
+            }
+        };
+
+        KeyStoreKeyingDataProvider.KeyStorePasswordProvider kspp = new KeyStoreKeyingDataProvider.KeyStorePasswordProvider()
+        {
+            @Override
+            public char[] getPassword()
+            {
+                return "19111985".toCharArray();
+            }
+        };
+
+        KeyStoreKeyingDataProvider.KeyEntryPasswordProvider kepp = new KeyStoreKeyingDataProvider.KeyEntryPasswordProvider()
+        {
+            @Override
+            public char[] getPassword(String s, X509Certificate x509Certificate)
+            {
+                return "19111985".toCharArray();
+            }
+        };
+
+        KeyingDataProvider keyingDataProvider = null;
+        try
+        {
+            keyingDataProvider = new PKCS11KeyStoreKeyingDataProvider(
+                    "/Users/luigi/Downloads/libbit4ipki.so",
+                    "SmartCard",
+                    scs,
+                    kspp, kepp,
+                    true);
+            XadesSigningProfile signingProfile = new XadesBesSigningProfile(keyingDataProvider)
+                    .withBasicSignatureOptionsProvider(new BasicSignatureOptionsProvider()
+                    {
+                        @Override
+                        public boolean includeSigningCertificate()
+                        {
+                            return true;     // aggiunge il SigningCertificate e il keyInfo
+                        }
+
+                        @Override
+                        public boolean includePublicKey()
+                        {
+                            return false;
+                        }
+
+                        @Override
+                        public boolean signSigningCertificate()
+                        {
+                            return true;   // firma il keyinfo
+                        }
+                    });
+            XadesSigner signer = signingProfile.newSigner();
+            return signer;
+        } catch (KeyStoreException e)
+        {
+            e.printStackTrace();
+        } catch (XadesProfileResolutionException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
